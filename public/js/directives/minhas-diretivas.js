9de240b34b0dfe42ddb7f98520a8be358b9e6f5c
dudulive <edurodriguesbraz@hotmail.com>
2018-11-19 21:32:05 -0200

    Correções

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,16 +1,32 @@
 angular.module('minhasDiretivas', [])
-    .directive('meuPainel', function() {
+	.directive('meuPainel', function() {
+
+		var ddo = {};
+
+		ddo.restrict = "AE";
+        ddo.transclude = true;
+
+
+		ddo.scope = {
+            titulo: '@'
+        };
+
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+
+		return ddo;
+	})
+    .directive('minhaFoto', function() {
 
         var ddo = {};
 
         ddo.restrict = "AE";
-        ddo.transclude = true;
 
         ddo.scope = {
-            titulo: '@'
+            titulo: '@',
+            url: '@'
         };
 
-        ddo.templateUrl = 'js/directives/meu-painel.html';           
-
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
         return ddo;
-});
+    });
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,17 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
 
-        $locationProvider.html5Mode(true);
+		$locationProvider.html5Mode(true);
 
-        $routeProvider.when('/fotos', {
-            templateUrl: 'partials/principal.html',
-            controller: 'FotosController'
-        });
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
 
-       // foto no singular!
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html'
+		});
 
-        $routeProvider.when('/fotos/new', {
-            templateUrl: 'partials/foto.html'
-        });
+		$routeProvider.otherwise({redirectTo: '/fotos'});
 
-        $routeProvider.otherwise({redirectTo: '/fotos'});
-
-    });
+	});
